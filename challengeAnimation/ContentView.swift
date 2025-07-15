5b8fa0563f217440d4f23fe19b8abdf21d230d8c
Acuiza <Acuiza@hypernovalabs.com>
2025-07-14 22:14:48 -0400

    Initial Commit

---
+++ challengeAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  challengeAnimation
+//
+//  Created by Alvaro Cuiza on 14/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ challengeAnimation/challengeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  challengeAnimationApp.swift
+//  challengeAnimation
+//
+//  Created by Alvaro Cuiza on 14/7/25.
+//
+
+import SwiftUI
+
+@main
+struct challengeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
