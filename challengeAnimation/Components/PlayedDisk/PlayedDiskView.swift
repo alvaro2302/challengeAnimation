2718324be4f4dd01d55f912dfc2fa1ac5a9c7dff
Acuiza <Acuiza@hypernovalabs.com>
2025-07-15 13:59:09 -0400

    add view for selected disk

--- challengeAnimation/Components/PlayedDisk/PlayedDiskView.swift
+++ challengeAnimation/Components/PlayedDisk/PlayedDiskView.swift
@@ -6,13 +6,40 @@
 //
 
 import SwiftUI
-
+var colorSpaceGray: Color = Color(red: 232/255, green: 230/255, blue: 233/255, opacity: 1)
+var colorDisk: Color = Color(red: 178/255, green: 175/255, blue: 178/255, opacity: 1)
+var colorDiskCenter: Color = Color(red: 170/255, green: 177/255, blue: 170/255, opacity: 1)
 struct PlayedDiskView: View {
+    @Binding var isSelecteDisk: Int?
+    var diskAnimation: Namespace.ID
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            ZStack{
+                Circle().fill(colorDisk).padding(10)
+                Circle().fill(colorDiskCenter).frame(width: 20, height: 20).padding(10)
+                
+                ForEach(1..<7, id: \.self) { diskId in
+                                  if isSelecteDisk == diskId {
+                                      DiskView(imageName: diskImageName(for: diskId))
+                                          .matchedGeometryEffect(id: diskId, in: diskAnimation)
+                                  }
+                              }
+          
+               
+            }
+          
+            
+        }.frame(width: 250, height: 250).background(colorSpaceGray).cornerRadius(50).shadow(radius: 10).opacity(0.9)
+    }
+    private func diskImageName(for id: Int) -> String {
+        print("chnage disk \(id)")
+        return "diskExample-\(id)"
     }
 }
-
+//232 230 233
 #Preview {
-    PlayedDiskView()
+    @State var isSelecteDisk: Int? = 4
+    @Namespace var diskAnimation
+    PlayedDiskView(isSelecteDisk: $isSelecteDisk, diskAnimation: diskAnimation)
 }
